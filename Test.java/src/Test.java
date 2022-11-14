a1ad337a177278fda71d18877e81b5ad0d19ff8b
saibaba <saibaba@DESKTOP-V6QGP3P>
2022-11-14 22:12:50 +0530

    update

---
+++ Test.java/src/Test.java
@@ -0,0 +1,77 @@
+import java.io.*;
+public class Test {
+	
+	//instance variable
+	String color;
+	String name;
+	String breed;
+	
+	
+	
+    //Default constructor
+	public Test() {
+		System.out.println("default constructor invoked");
+	}
+	
+	
+    //Parameterized constructor
+	public Test(String color, String name, String breed) {
+		System.out.println("parameterized constructor invoked");
+		this.color = color;
+		this.name = name;
+		this.breed = breed;
+	}
+	
+	//Copy constructor
+	public Test(Test obj)
+	{
+		System.out.println("copy constructor invoked");
+		this.color = obj.color;
+		this.name = obj.name;
+		this.breed = obj.breed;
+	}
+
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	//methods
+	public void waggingTail()
+	{
+		System.out.println("This is waggingTail method");
+	}
+	
+	public void eating()
+	{
+		System.out.println("This is eat method");
+	}
+	
+	public void barking()
+	{
+		System.out.println("This is barking method");
+	}
+	
+	
+
+}
